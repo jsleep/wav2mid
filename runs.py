c55414eb41d48494fa6d5b098c426790e2f3b144
Brian Pow <brianpow@gmail.com>
2020-11-27 20:08:50 +0800

    add multiprocessing

--- runs.py
+++ runs.py
@@ -1,14 +1,31 @@
 import csv
 from config import create_config
 from preprocess import preprocess, organize
-from keras_train import train
+import multiprocessing
+import argparse
 
-with open('models.csv', 'r') as csvfile:
-     reader = csv.DictReader(csvfile)
-     #print header
-     for args in reader:
-         print(args['model_name'])
-         create_config(args)
-         preprocess(args)
-         organize(args)
-         train(args)
+def jobs(args):
+    from keras_train import train
+
+    print("Working on {}...".format(args['model_name']))
+    create_config(args)
+    preprocess(args)
+    organize(args)
+    train(args)
+
+def main(pool):
+    with open('models.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        pool.map(jobs, reader)
+        return
+
+if __name__== '__main__':
+    # semaphore = multiprocessing.Semaphore(1)
+    counts=multiprocessing.cpu_count()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--threads', metavar='N',type=int, required=False, help='Numbers of threads for models (default=%(default)s, max={})'.format(str(counts)),default=1)
+
+    args = vars(parser.parse_args())
+
+    pool=multiprocessing.Pool(args['threads'])
+    main(pool)
